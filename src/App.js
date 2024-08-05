71b91f2c68db48628093b16c4f1edaaf4d56447e
mohdreyhan <42486329+mohdreyhan@users.noreply.github.com>
2024-08-05 17:26:32 -0400

    Added lint and jest config x 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  //changes to verify build in jenkins
+  //changes to verify build in jenkins once again
+  
   return (
     <div className="App">
       <header className="App-header">

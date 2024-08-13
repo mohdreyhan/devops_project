57d696982005ee8bd7554b61902b10b03fa019cf
mohdreyhan <42486329+mohdreyhan@users.noreply.github.com>
2024-08-12 21:23:06 -0400

    testing docker

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React10
+          Learn React11
         </a>
       </header>
     </div>

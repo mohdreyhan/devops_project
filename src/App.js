714fa641d8b68f3da95dec99abc8e1eefef7e872
mohdreyhan <42486329+mohdreyhan@users.noreply.github.com>
2024-08-13 18:23:55 -0400

    testing kubernetes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React12
+          Learn React13
         </a>
       </header>
     </div>

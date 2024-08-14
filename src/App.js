b9a762b22f3a912ed7633b504ded10acade0bfd6
Mohammed Reyhan Ur Rab <42486329+mohdreyhan@users.noreply.github.com>
2024-08-13 21:09:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React restart container
+          Learn React skipping container
         </a>
       </header>
     </div>

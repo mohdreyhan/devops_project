f0ea7730aa2b2fc30afbf819cc8f66efbca65d57
mohdreyhan <42486329+mohdreyhan@users.noreply.github.com>
2024-08-16 12:17:07 -0400

    round 2 testing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in class
+          Learn React in class - part 2
         </a>
       </header>
     </div>
